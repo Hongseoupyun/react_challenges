eb1ea3340bf0c469717514ac3973f5bb93c07b0d
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-09-17 16:10:19 -0400

    added logic

--- react_challenges/src/Components/ImageCarousal.jsx
+++ react_challenges/src/Components/ImageCarousal.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
-import imgData from "../imgData.json"
+import React from "react";
+import imgData from "../imgData.json";
+import "./ImageCarousal.css";
+import { useState, useEffect } from "react";
 
 export default function ImageCarousal() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const length = imgData.length;
 
+  const nextSlide = function () {
+    setCurrentPage(currentPage === length - 1 ? 0 : currentPage + 1);
+  };
 
+  const prevSlide = function () {
+    setCurrentPage(currentPage === 0 ? length - 1 : currentPage - 1);
+  };
 
+  if (!Array.isArray(imgData) || imgData.length === 0) {
+    return null;
+  }
 
+  console.log(currentPage);
   return (
-    <div>{imgData.map((element)=>{
-      return(
-        <img src={element.image} key={element.id} alt="baby image"/>
-      )
-    })}</div>
-  )
+    <div>
+      <section className="slider">
+        {imgData.map((element, index) => {
+          return (
+            <div
+              className={index === currentPage ? "slide active" : "slide"}
+              key={element.id}
+            >
+              {index === currentPage && (
+                <img src={element.image} alt="baby image" className="image" />
+              )}
+            </div>
+          );
+        })}
+        <button className="left" onClick={prevSlide}>
+          Previous
+        </button>
+        <button className="right" onClick={nextSlide}>
+          Next
+        </button>
+      </section>
+    </div>
+  );
 }
-
