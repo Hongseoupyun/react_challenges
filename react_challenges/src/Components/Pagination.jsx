0fb2488883b497b26c883ea31ba78602193eece7
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-09-18 22:31:29 -0400

    added posts components

--- react_challenges/src/Components/Pagination.jsx
+++ react_challenges/src/Components/Pagination.jsx
@@ -1,10 +1,27 @@
+import axios from "axios";
 import React from "react";
-import { useState } from "react";
-import jsonData from "../MOCK_DATA.json";
+import { useEffect, useState } from "react";
+// import jsonData from "../MOCK_DATA.json";
 
 export default function Pagination() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerpage] = useState(10);
+  const url = "https://jsonplaceholder.typicode.com/posts";
 
-  const [users,setUsers] = useState(jsonData.slice(0,50))
+  useEffect(() => {
+    const fetchedPost = async function () {
+      setLoading(true);
+      const res = await axios.get(url);
+      setPosts(res);
+      setLoading(false);
+    };
+    fetchedPost();
+  }, []);
 
-  return <div>Pagination</div>;
+  console.log(posts)
+  return (<div className="container mt-5">
+    <h1 className="text-primary mb-3">Pagination...!</h1>
+  </div>);
 }
